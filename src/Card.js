c824ba94f1e93e86ea8c5201ac13bbfdf7ccebff
weixuefeng <weixuefeng1018@gmail.com>
2020-11-05 17:13:31 +0800

    update page

--- src/Card.js
+++ src/Card.js
@@ -8,12 +8,17 @@ class Card extends React.Component{
 
   render(){
     return (
-      <a href={this.props.target}>
-      <div className="Col center center-vertical" style={{ height: '200px', background: '#4dd0e1', margin: '20px'}}
-      >
-        <p>{this.props.name}</p>
+      <div className="Col center" style={{ marginTop: '20px'}}>
+        <div className="token-card text-center">
+        <a href={this.props.target}>
+        <div className="Col center center-vertical" style={{ height: '200px', width: '200px', margin: '10px'}}
+        >
+          <p>{this.props.name}</p>
+        </div>
+        </a>
+        </div>
       </div>
-      </a>
+
     )
   }
 }
